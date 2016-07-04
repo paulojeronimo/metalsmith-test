56ca2cc562dca2e318a4fa6aebd7ded724eef8f8
Paulo Jerônimo <paulojeronimo@gmail.com>
2016-07-04 20:51:56 +0000

    Desenvolvido o uso de layouts

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ var path = require('path');
 var metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
 var assets = require('metalsmith-assets');
+var layouts = require('metalsmith-layouts');
 
 metalsmith(__dirname)
     .source('content')
     .destination('dist')
     .use(markdown())
+    .use(layouts({
+	engine: 'jade',
+        default: 'default.jade',
+	pattern: '**/*.html'
+    }))
     .use(assets({
 	source: 'assets'
     }))
