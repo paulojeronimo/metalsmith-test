1df9a1845cb82ccb177464498f5b97b08576fcca
Paulo Jerônimo <paulojeronimo@gmail.com>
2016-07-04 20:50:37 +0000

    Feito o uso de arquivos Markdown

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var path = require('path');
 
 var metalsmith = require('metalsmith');
+var markdown = require('metalsmith-markdown');
 
 metalsmith(__dirname)
     .source('content')
     .destination('dist')
+    .use(markdown())
     .build(function (err) {
         if (err) { console.error(err); }
     });
