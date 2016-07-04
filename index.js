99a709efb761af4dd02c15fe3b6602cb2a5071fc
Paulo Jerônimo <paulojeronimo@gmail.com>
2016-07-04 20:51:11 +0000

    Disponibilizado arquivos estáticos

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ var path = require('path');
 
 var metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
+var assets = require('metalsmith-assets');
 
 metalsmith(__dirname)
     .source('content')
     .destination('dist')
     .use(markdown())
+    .use(assets({
+	source: 'assets'
+    }))
     .build(function (err) {
         if (err) { console.error(err); }
     });
